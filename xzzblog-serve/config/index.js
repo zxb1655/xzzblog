01dbf5db92274e290fbb2638085880c25119d549
zxb1655 <58070684+zxb1655@users.noreply.github.com>
2022-01-10 13:37:38 +0800

    更新README

--- xzzblog-serve/config/index.js
+++ xzzblog-serve/config/index.js
@@ -6,9 +6,9 @@ module.exports = {
   SECRET_KEY: "zxbzxb",
   NODEMAILER: {
     email: "204006387@qq.com",
-    pass: "",
-    IMAP: "", // qq 授权码 不是qq密码
-    POP3: "",
+    pass: "yanuullzyvaubjie",
+    IMAP: "xlsjvgllhynabiab", // qq 授权码 不是qq密码
+    POP3: "yanuullzyvaubjie",
   },
   MYSQL: {
     HOST: "127.0.0.1",
@@ -19,8 +19,8 @@ module.exports = {
 
   oss: {
     region: "oss-cn-guangzhou",
-    accessKeyId: "",
-    accessKeySecret: "",
+    accessKeyId: "LTAI4GJPjQzRS25WpBKvyuM9",
+    accessKeySecret: "VLv1IFwFO6PUlMq5zOXBvwaAa0DxfP",
     bucket: "zxbblog",
   },
 
